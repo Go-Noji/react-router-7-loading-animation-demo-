1e33d56a6f411e42a4a63d98254cc3ed189a8c86
Go Noji <gisosyadfe@gmail.com>
2025-02-02 11:45:54 +0900

    [fix] basename の設定を忘れていたので追加
    GitHub のリポジトリ名に合わせる

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/react-router-7-loading-animation-demo-/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
